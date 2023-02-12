f01eba9db7a78cd5d1c83465469a577b369793f0
SebastianGC94 <sebasgc.9447@gmail.com>
2023-02-12 12:44:45 -0500

    Cambios 12/02/2023

--- js/slider.js
+++ js/slider.js
@@ -29,3 +29,35 @@
     }
 
 })();
+
+(function(){
+    
+    const sliders2 = [...document.querySelectorAll('.testimony__body2')];
+    const buttonNext = document.querySelector('#next2');
+    const buttonBefore = document.querySelector('#before2');
+    let value;   
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition2(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition2(-1);
+    });
+
+    const changePosition2 = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show2').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+
+
+        sliders2[Number(currentTestimony)-1].classList.remove('testimony__body--show2');
+        if(value === sliders2.length+1 || value === 0){
+            value = value === 0 ? sliders2.length  : 1;
+        }
+
+        sliders2[value-1].classList.add('testimony__body--show2');
+
+    }
+
+})();
